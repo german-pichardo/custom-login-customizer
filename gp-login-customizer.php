ec6b5d802bb83402322c444c596287d5ee43890b
gpichardo <gpichardob@gmail.com>
2018-05-10 12:20:54 +0200

    code format

--- gp-login-customizer.php
+++ gp-login-customizer.php
@@ -8,10 +8,10 @@
  * Text Domain: custom-login-settings
  */
 // If this file is called directly, abort.
-if ( !defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
     die;
 }
-if ( !class_exists( ' GpLoginCustomizer' ) ) {
+if (!class_exists(' GpLoginCustomizer')) {
     class  GpLoginCustomizer
     {
         private $section_handle = 'custom_login_section';
@@ -20,14 +20,14 @@ if ( !class_exists( ' GpLoginCustomizer' ) ) {
 
         public function __construct()
         {
-            add_action( 'admin_menu', array( $this, 'add_menu_item' ) );
+            add_action('admin_menu', [$this, 'add_menu_item']);
             // ADMIN : Register customize options
-            add_action( 'customize_register', array( $this, 'register_settings' ) );
-            add_action( 'login_head', array( $this, 'logo_image' ) );
-            add_action( 'login_headerurl', array( $this, 'logo_url' ) );
-            add_action( 'login_headertitle', array( $this, 'logo_title' ) );
-            add_action( 'login_errors', array( $this, 'error_message' ) );
-            add_action( 'login_head', array( $this, 'custom_login_css' ) );
+            add_action('customize_register', [$this, 'register_settings']);
+            add_action('login_head', [$this, 'logo_image']);
+            add_action('login_headerurl', [$this, 'logo_url']);
+            add_action('login_headertitle', [$this, 'logo_title']);
+            add_action('login_errors', [$this, 'error_message']);
+            add_action('login_head', [$this, 'custom_login_css']);
         }
 
         /*
@@ -36,176 +36,176 @@ if ( !class_exists( ' GpLoginCustomizer' ) ) {
          */
         public function add_menu_item()
         {
-            if ( is_multisite() && is_network_admin() ) return; // Do not use in main_network
+            if (is_multisite() && is_network_admin()) return; // Do not use in main_network
 
             $menu_slug_url = 'customize.php?autofocus[section]=' . $this->section_handle . '';
             // We add simple autoFocus in multiSite - Url redirection do not play well
-            if ( !is_multisite() ) {
-                $menu_slug_url .= '&url=' . urlencode( wp_login_url() ) . '&return=' . urlencode( wp_login_url() ) . '';
+            if (!is_multisite()) {
+                $menu_slug_url .= '&url=' . urlencode(wp_login_url()) . '&return=' . urlencode(wp_login_url()) . '';
             }
-            add_theme_page( __( 'Login Customizer', self::$text_domain ), __( 'Login Customizer', self::$text_domain ), 'manage_options', '' . $menu_slug_url . '' );
+            add_theme_page(__('Login Customizer', self::$text_domain), __('Login Customizer', self::$text_domain), 'manage_options', '' . $menu_slug_url . '');
         }
         //  =====================================================
         //  = ADMIN: Customize options                          =
         //  =====================================================
-        public function register_settings( $wp_customize )
+        public function register_settings($wp_customize)
         {
-            $wp_customize->add_section( $this->section_handle, array(
-                'title'    => __( 'Login Customizer', self::$text_domain ),
+            $wp_customize->add_section($this->section_handle, [
+                'title' => __('Login Customizer', self::$text_domain),
                 'priority' => 35,
-            ) );
+            ]);
 
             //  =====================================================
             //  = Image Upload    setting_logo_image          =
             //  =====================================================
-            $wp_customize->add_setting( 'setting_logo_image', array(
-                'default' => has_site_icon() ? get_site_icon_url( 150 ) : esc_url( get_site_icon_url( 64, admin_url( 'images/w-logo-blue.png' ) ) ),
-            ) );
+            $wp_customize->add_setting('setting_logo_image', [
+                'default' => has_site_icon() ? get_site_icon_url(150) : esc_url(get_site_icon_url(64, admin_url('images/w-logo-blue.png'))),
+            ]);
 
-            $wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'setting_logo_image', array(
-                'label'       => __( 'Login logo', self::$text_domain ),
-                'description' => __( 'Ideal size: squared transparent png 110x110px, maximum size 150px  )', self::$text_domain ),
-                'section'     => $this->section_handle,
-                'settings'    => 'setting_logo_image',
-            ) ) );
+            $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'setting_logo_image', [
+                'label' => __('Login logo', self::$text_domain),
+                'description' => __('Ideal size: squared transparent png 110x110px, maximum size 150px  )', self::$text_domain),
+                'section' => $this->section_handle,
+                'settings' => 'setting_logo_image',
+            ]));
 
             //  =====================================================
             //  = Color Picker : setting_login_body_background      =
             //  =====================================================
-            $wp_customize->add_setting( 'setting_login_body_background', array(
-                'default'           => '#e8e8e7',
+            $wp_customize->add_setting('setting_login_body_background', [
+                'default' => '#e8e8e7',
                 'sanitize_callback' => 'sanitize_hex_color',
-            ) );
+            ]);
 
-            $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'setting_login_body_background', array(
-                'label'    => __( 'Body background color', self::$text_domain ),
-                'section'  => $this->section_handle,
+            $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'setting_login_body_background', [
+                'label' => __('Body background color', self::$text_domain),
+                'section' => $this->section_handle,
                 'settings' => 'setting_login_body_background',
-            ) ) );
+            ]));
 
             //  =====================================================
             //  = Color Picker : setting_form_label_color           =
             //  =====================================================
-            $wp_customize->add_setting( 'setting_form_label_color', array(
-                'default'           => '#72777c',
+            $wp_customize->add_setting('setting_form_label_color', [
+                'default' => '#72777c',
                 'sanitize_callback' => 'sanitize_hex_color',
-            ) );
+            ]);
 
-            $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'setting_form_label_color', array(
-                'label'    => __( 'Body Default Font Colo', self::$text_domain ),
-                'section'  => $this->section_handle,
+            $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'setting_form_label_color', [
+                'label' => __('Body Default Font Colo', self::$text_domain),
+                'section' => $this->section_handle,
                 'settings' => 'setting_form_label_color',
-            ) ) );
+            ]));
 
             //  =====================================================
             //  = Text Input setting_error_message    =
             //  =====================================================
-            $wp_customize->add_setting( 'setting_error_message', array(
+            $wp_customize->add_setting('setting_error_message', [
                 'default' => 'ERROR: Incorrect login details.',
-            ) );
+            ]);
 
-            $wp_customize->add_control( 'setting_error_message', array(
-                'label'       => __( 'Error message', self::$text_domain ),
-                'description' => __( 'For security reasons it\'s better to insert a generic message instead of precising "Invalid username" or "Invalid password".', self::$text_domain ),
-                'section'     => $this->section_handle,
-                'settings'    => 'setting_error_message',
-                'type'        => 'text',
+            $wp_customize->add_control('setting_error_message', [
+                'label' => __('Error message', self::$text_domain),
+                'description' => __('For security reasons it\'s better to insert a generic message instead of precising "Invalid username" or "Invalid password".', self::$text_domain),
+                'section' => $this->section_handle,
+                'settings' => 'setting_error_message',
+                'type' => 'text',
 
-            ) );
+            ]);
 
             //  =====================================================
             //  = Color Picker : setting_form_button_text_color     =
             //  =====================================================
-            $wp_customize->add_setting( 'setting_form_button_text_color', array(
-                'default'           => '#ffffff',
+            $wp_customize->add_setting('setting_form_button_text_color', [
+                'default' => '#ffffff',
                 'sanitize_callback' => 'sanitize_hex_color',
-            ) );
+            ]);
 
-            $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'setting_form_button_text_color', array(
-                'label'    => __( 'Button text color', self::$text_domain ),
-                'section'  => $this->section_handle,
+            $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'setting_form_button_text_color', [
+                'label' => __('Button text color', self::$text_domain),
+                'section' => $this->section_handle,
                 'settings' => 'setting_form_button_text_color',
-            ) ) );
+            ]));
 
             //  =====================================================
             //  = Color Picker : setting_form_primary_color         =
             //  =====================================================
-            $wp_customize->add_setting( 'setting_form_primary_color', array(
-                'default'           => '#293550',
+            $wp_customize->add_setting('setting_form_primary_color', [
+                'default' => '#293550',
                 'sanitize_callback' => 'sanitize_hex_color',
-            ) );
+            ]);
 
-            $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'setting_form_primary_color', array(
-                'label'    => __( 'Button background color', self::$text_domain ),
-                'section'  => $this->section_handle,
+            $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'setting_form_primary_color', [
+                'label' => __('Button background color', self::$text_domain),
+                'section' => $this->section_handle,
                 'settings' => 'setting_form_primary_color',
-            ) ) );
+            ]));
 
             //  =====================================================
             //  = Color Picker : setting_form_input_border_color    =
             //  =====================================================
-            $wp_customize->add_setting( 'setting_form_input_border_color', array(
-                'default'           => '#e3e5e8',
+            $wp_customize->add_setting('setting_form_input_border_color', [
+                'default' => '#e3e5e8',
                 'sanitize_callback' => 'sanitize_hex_color',
-            ) );
+            ]);
 
-            $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'setting_form_input_border_color', array(
-                'label'    => __( 'Input field color', self::$text_domain ),
-                'section'  => $this->section_handle,
+            $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'setting_form_input_border_color', [
+                'label' => __('Input field color', self::$text_domain),
+                'section' => $this->section_handle,
                 'settings' => 'setting_form_input_border_color',
-            ) ) );
+            ]));
 
             //  =====================================================
             //  = Color Picker : setting_form_input_border_width    =
             //  =====================================================
-            $wp_customize->add_setting( 'setting_form_input_border_width', array( 'default' => '1px' ) );
-            $wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'setting_form_input_border_width', array(
-                    'label'    => __( 'Input field border width', self::$text_domain ),
-                    'section'  => $this->section_handle,
+            $wp_customize->add_setting('setting_form_input_border_width', ['default' => '1px']);
+            $wp_customize->add_control(new WP_Customize_Control($wp_customize, 'setting_form_input_border_width', [
+                    'label' => __('Input field border width', self::$text_domain),
+                    'section' => $this->section_handle,
                     'settings' => 'setting_form_input_border_width',
-                    'type'     => 'select',
-                    'choices'  => array(
+                    'type' => 'select',
+                    'choices' => [
                         '0px' => '0px',
                         '1px' => '1px',
                         '2px' => '2px',
-                    )
-                )
-            ) );
+                    ]
+                ]
+            ));
 
             //  =====================================================
             //  = Color Picker : setting_form_secondary_color       =
             //  =====================================================
-            $wp_customize->add_setting( 'setting_form_secondary_color', array(
-                'default'           => '#ffcc4d',
+            $wp_customize->add_setting('setting_form_secondary_color', [
+                'default' => '#ffcc4d',
                 'sanitize_callback' => 'sanitize_hex_color',
-            ) );
+            ]);
 
-            $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'setting_form_secondary_color', array(
-                'label'       => __( 'Secondary color', self::$text_domain ),
-                'description' => __( 'Messages border and Checkbox', self::$text_domain ),
-                'section'     => $this->section_handle,
-                'settings'    => 'setting_form_secondary_color',
+            $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'setting_form_secondary_color', [
+                'label' => __('Secondary color', self::$text_domain),
+                'description' => __('Messages border and Checkbox', self::$text_domain),
+                'section' => $this->section_handle,
+                'settings' => 'setting_form_secondary_color',
 
-            ) ) );
+            ]));
 
             //  =====================================================
             //  = Text Input setting_additional_css    =
             //  =====================================================
-            $wp_customize->add_setting( 'setting_additional_css', array(
+            $wp_customize->add_setting('setting_additional_css', [
                 'default' => '/*You can add your own CSS here.*/',
-            ) );
+            ]);
 
-            $wp_customize->add_control( 'setting_additional_css', array(
-                'label'       => __( 'Additional CSS', self::$text_domain ),
-                'description' => __( '', self::$text_domain ),
-                'section'     => $this->section_handle,
-                'settings'    => 'setting_additional_css',
-                'type'        => 'textarea',
-                'input_attrs' => array(
+            $wp_customize->add_control('setting_additional_css', [
+                'label' => __('Additional CSS', self::$text_domain),
+                'description' => __('', self::$text_domain),
+                'section' => $this->section_handle,
+                'settings' => 'setting_additional_css',
+                'type' => 'textarea',
+                'input_attrs' => [
                     'class' => 'code',
-                )
+                ]
 
-            ) );
+            ]);
 
         }
 
@@ -216,12 +216,12 @@ if ( !class_exists( ' GpLoginCustomizer' ) ) {
         public static function logo_image()
         {
 
-            $logo_image = get_theme_mod( 'setting_logo_image', has_site_icon() ? get_site_icon_url( 150 ) : esc_url( get_site_icon_url( 64, '' ) ) );
-            if ( !empty($logo_image) ) {
+            $logo_image = get_theme_mod('setting_logo_image', has_site_icon() ? get_site_icon_url(150) : esc_url(get_site_icon_url(64, admin_url('images/w-logo-blue.png'))));
+            if (!empty($logo_image)) {
                 $logo_image_default_size = '150px';
-                $logo_image_size = getimagesize( $logo_image );
-                $logo_image_width = $logo_image_size[ 0 ] . 'px';
-                $logo_image_height = $logo_image_size[ 1 ] . 'px';
+                $logo_image_size = getimagesize($logo_image);
+                $logo_image_width = $logo_image_size[0] . 'px';
+                $logo_image_height = $logo_image_size[1] . 'px';
 
                 echo '<style type="text/css">
                     body.login h1 a {
@@ -248,13 +248,13 @@ if ( !class_exists( ' GpLoginCustomizer' ) ) {
         // Change default logo title attribute "Powered by Wordpress"
         public static function logo_title()
         {
-            return get_option( 'blogname' );
+            return get_option('blogname');
         }
 
         // For security reasons it's better to insert a generic message instead of precising "Invalid username" or "Invalid password".
         public static function error_message()
         {
-            return get_theme_mod( 'setting_error_message', __( 'ERROR: Incorrect login details.', self::$text_domain ) );
+            return get_theme_mod('setting_error_message', __('ERROR: Incorrect login details.', self::$text_domain));
         }
 
         // Add custom css styles : external css or inline css to overwrite default form styles
@@ -331,17 +331,17 @@ if ( !class_exists( ' GpLoginCustomizer' ) ) {
             <?php
         }
 
-        public static function generate_css( $selector, $style, $mod_name, $fallback_value, $prefix = '', $postfix = '', $echo = true )
+        public static function generate_css($selector, $style, $mod_name, $fallback_value, $prefix = '', $postfix = '', $echo = true)
         {
             $return = '';
-            $mod = get_theme_mod( $mod_name, $fallback_value );
-            if ( !empty($mod) ) {
-                $return = sprintf( '%s { %s:%s; }',
+            $mod = get_theme_mod($mod_name, $fallback_value);
+            if (!empty($mod)) {
+                $return = sprintf('%s { %s:%s; }',
                     $selector,
                     $style,
                     $prefix . $mod . $postfix
                 );
-                if ( $echo ) {
+                if ($echo) {
                     echo $return;
                 }
             }
