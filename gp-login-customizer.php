690ff7a7899c7b591eaa810bda3904c919c44f94
German Pichardo <german-pichardo@users.noreply.github.com>
2018-05-19 13:10:40 +0200

    Update version

--- gp-login-customizer.php
+++ gp-login-customizer.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Gp Login Customizer
  * Description: Change default login URL, Title, Styles, Logo, etc. Go to : Appearance -> Themes -> Customize -> Login page
- * Version: 1.0.1
+ * Version: 1.2.0
  * Author: German Pichardo
  * Author URI: http://www.german-pichardo.com
  * Text Domain: custom-login-settings
