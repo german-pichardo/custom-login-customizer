40be9222447ee1a0fd28680fbb4d308dc9dcaeeb
gpichardo <gpichardob@gmail.com>
2021-01-31 14:09:51 +0100

    Update language

--- includes/class-i18n.php
+++ includes/class-i18n.php
@@ -27,7 +27,7 @@ class I18n {
 	 */
 	public function load_plugin_textdomain() {
 		load_plugin_textdomain(
-			'apicil',
+			'gp',
 			false,
 			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
 		);
